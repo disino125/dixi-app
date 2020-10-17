eac3601d05e7566aa0b4377baecd96758fb8ee71
disino_glb <disino1125@hotmail.com>
2020-10-17 15:30:28 +0200

    my exercise-001 version 04

--- src/main/java/de/dixi/app/WordCounter.java
+++ src/main/java/de/dixi/app/WordCounter.java
@@ -11,7 +11,26 @@ public class WordCounter {
     private WordCounter() {
     }
 
-    public static String[] getAlphabetsFromText(String text){
+    public static String getMostWordOccurences(String text) {
+
+        if (text == null || text.equals("")) {
+            return "";
+        }
+
+        String[] strs = getAlphabetsFromText(text);
+
+        HashSet<String> set = setToFilterSameWords(strs);
+
+        HashMap<String, Integer> map = compareSetAndStringsAndSaveCountAndWordInMap(strs, set);
+
+        ArrayList<Map.Entry<String, Integer>> list = putMapEntryInListAndSort(map);
+
+        return getPrintStringForm(list);
+        //throw new UnsupportedOperationException("Not implemented.");
+    }
+
+
+    private static String[] getAlphabetsFromText(String text) {
         Pattern pattern = Pattern.compile("[^(A-ZÄÖÜa-zäöüß)]");
         Matcher matcher = pattern.matcher(text);
         String result = matcher.replaceAll(" ");
@@ -19,7 +38,7 @@ public class WordCounter {
         return strs;
     }
 
-    public static HashSet<String> Set_to_filterSameWords(String[] strings){
+    private static HashSet<String> setToFilterSameWords(String[] strings) {
         HashSet<String> set = new HashSet<>();
         for (String str : strings) {
             set.add(str);
@@ -27,30 +46,30 @@ public class WordCounter {
         return set;
     }
 
-    public static HashMap<String, Integer> CompareSetAndStrings_And__save_CountAndWord_In_Map
-            (String[] strs, HashSet<String> set){
+    private static HashMap<String, Integer> compareSetAndStringsAndSaveCountAndWordInMap
+            (String[] strs, HashSet<String> set) {
         HashMap<String, Integer> map = new HashMap<>();
         Iterator<String> it = set.iterator();
-        while (it.hasNext()){
+        while (it.hasNext()) {
             String next = it.next();
-            int count=0;
+            int count = 0;
             for (String str : strs) {
-                if(str.equals(next)){
+                if (str.equals(next)) {
                     count++;
                 }
             }
-            map.put(next,count);
+            map.put(next, count);
         }
         return map;
     }
 
-    public static ArrayList<Map.Entry<String, Integer>> putMapEntryInList_and_Sort
-            (HashMap<String, Integer> map){
+    private static ArrayList<Map.Entry<String, Integer>> putMapEntryInListAndSort
+            (HashMap<String, Integer> map) {
         ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if(o2.getValue()==o1.getValue()){
+                if (o2.getValue() == o1.getValue()) {
                     return o1.getKey().compareTo(o2.getKey());
                 }
                 return o2.getValue() - o1.getValue();
@@ -59,32 +78,14 @@ public class WordCounter {
         return list;
     }
 
-    public static String getPrintStringForm(ArrayList<Map.Entry<String, Integer>> list){
-        String counter="";
+    private static String getPrintStringForm(ArrayList<Map.Entry<String, Integer>> list) {
+        String counter = "";
         for (Map.Entry<String, Integer> entry : list) {
-            counter+= StringUtils.leftPad(entry.getValue().toString(), 3, "0")+
-                    " "+entry.getKey()+"\n";
+            counter += StringUtils.leftPad(entry.getValue().toString(), 3, "0") +
+                    " " + entry.getKey() + "\n";
         }
-        return  counter;
+        return counter;
     }
 
-
-    public static String getMostWordOccurences(String text) {
-
-        if(text==null||text.equals("")){
-            return "";
-        }
-
-        String[] strs = getAlphabetsFromText(text);
-
-        HashSet<String> set = Set_to_filterSameWords(strs);
-
-        HashMap<String, Integer> map = CompareSetAndStrings_And__save_CountAndWord_In_Map(strs, set);
-
-        ArrayList<Map.Entry<String, Integer>> list = putMapEntryInList_and_Sort(map);
-
-        return getPrintStringForm(list);
-        //throw new UnsupportedOperationException("Not implemented.");
-    }
 
 }
